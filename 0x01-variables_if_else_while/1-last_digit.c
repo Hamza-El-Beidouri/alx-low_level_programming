5bc79054a97e3185b90fb8940371b72377c41141
Hamza-El-Beidouri <hamzaelbeidouri296@gmail.com>
2023-07-14 13:31:19 -0700

    Description

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
- /**
- * main - Entry point of the program
- *
- *Description: 'the program's description'
-
- * Return: Always 0.
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
