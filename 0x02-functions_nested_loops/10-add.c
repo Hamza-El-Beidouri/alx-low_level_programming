bbc2ed16c895ec32fadc0c8522a9f3b41c956054
Hamza-El-Beidouri <hamzaelbeidouri296@gmail.com>
2023-07-18 14:15:56 -0700

    rask-10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,13 @@
 
 /**
  * add - Adds two integers.
- * @num1: The first integer to be added.
- * @num2: The second integer to be added.
+ * @y: The first integer to be added.
+ * @x: The second integer to be added.
  *
  * Return: The result of the addition.
  */
-int add(int x , int y)
+int add(int x, int y)
 {
-	return (x+y);
+	return (x + y);
 }
 
