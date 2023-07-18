9e065e656f181ffcdc344838a78f4f3c4a955f43
Hamza-El-Beidouri <hamzaelbeidouri296@gmail.com>
2023-07-18 12:54:51 -0700

    task-2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int line;
+	char ch;
+
+	for (line = 0; line <= 9; line++)
+{
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+		_putchar(ch);	
+}
+
+		_putchar('\n');
+
+
+
+
+}
