bfca2a5485cfafe73e8322313f901883ec61ee1f
Hamza-El-Beidouri <hamzaelbeidouri296@gmail.com>
2023-07-18 12:28:49 -0700

    task-1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,12 @@
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; i++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 {
 	_putchar(letter);
 }
