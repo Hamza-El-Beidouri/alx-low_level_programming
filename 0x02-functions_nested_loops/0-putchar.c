e223f5b571ef8a03f8bcbb120a44e6eb1aa47ced
Hamza-El-Beidouri <hamzaelbeidouri296@gmail.com>
2023-07-18 09:59:56 -0700

    task-0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/***
+ *
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+	char c1 = '_';
+	char c2 = 'p';
+	char c3 = 'u';
+	char c4 = 't';
+	char c5 = 'c';
+	char c6 = 'h';
+	char c7 = 'a';
+	char c8 = 'r';
+
+	putchar(c1);
+	putchar(c2);
+	putchar(c3);
+	putchar(c4);
+	putchar(c5);
+	putchar(c6);
+	putchar(c7);
+	putchar(c8);
+	putchar('\n');
+
+	return (0);
+}
