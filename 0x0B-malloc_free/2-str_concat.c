2be0639e91d9d5114b087efd45ed233b3c43174e
Hamza-El-Beidouri <hamzaelbeidouri296@gmail.com>
2023-08-08 09:45:00 -0700

    merging two strings using DMA

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -20,30 +19,28 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 != NULL)
 	{
-		i = 0;
-		while (s1[i] != '\0')
-			len1++;
-		i++;
+	i = 0;
+	while (s1[i++] != '\0')
+	len1++;
 	}
 
 	if (s2 != NULL)
 	{
-	i = 0;
-	while (s2[i] != '\0')
-	len1++;
-	i++;
+	j = 0;
+	while (s2[j++] != '\0')
+	len2++;
 	}
 
 	len = len1 + len2;
 	s = (char *)malloc(sizeof(char) * (len + 1));
 
 	if (s == NULL)
-		return (NULL);
+	return (NULL);
 
 	for (i = 0; i < len1; i++)
-		s[i] = s1[i];
+	s[i] = s1[i];
 	for (j = 0; j < len2; j++, i++)
-		s[i] = s2[j];
+	s[i] = s2[j];
 	s[len] = '\0';
 
 	return (s);
